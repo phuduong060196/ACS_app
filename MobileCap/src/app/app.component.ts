0f4097261510e14d99279e09394f8bb85ae748cc
PhuDuong <phuduong060196@gmail.com>
2018-10-15 00:37:44 +0700

    doi chut choi

--- MobileCap/src/app/app.component.ts
+++ MobileCap/src/app/app.component.ts
@@ -41,41 +41,14 @@ export class foodIonicApp {
 
     constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
         this.initializeApp();
-
         this.homeItem = { component: 'page-home' };
         this.messagesItem = { component: 'page-message-list' };
         this.cartItem = { component: 'page-cart' };
-
-
-        this.appMenuItems = [
-            { title: 'Restaurants', component: 'page-restaurant-list', icon: 'home' },
-            { title: 'Dish List', component: 'page-dish-list', icon: 'pizza' },
-            { title: 'Nearby', component: 'page-nearby', icon: 'compass' },
-            { title: 'By Category', component: 'page-category', icon: 'albums' },
-            { title: 'Latest Orders', component: 'page-orders', icon: 'list-box' },
-            { title: 'Cart', component: 'page-cart', icon: 'cart' },
-            { title: 'Favorite Restaurants', component: 'page-favorite-list', icon: 'heart' },
-            { title: 'Favorite Dishes', component: 'page-favorite-dish', icon: 'heart' }
-        ];
-
-        this.yourRestaurantMenuItems = [
-            { title: 'Register Restaurant', component: 'page-your-restaurant', icon: 'clipboard' }
-        ];
-
-
         this.accountMenuItems = [
-            { title: 'Login', component: 'page-auth', icon: 'log-in' },
-            { title: 'My Account', component: 'page-my-account', icon: 'contact' },
-            { title: 'Logout', component: 'page-auth', icon: 'log-out' },
+            { title: 'Đăng nhập', component: 'page-auth', icon: 'log-in' },
+            { title: 'Tài khoản', component: 'page-my-account', icon: 'contact' },
+            { title: 'Đăng xuất', component: 'page-auth', icon: 'log-out' },
         ];
-
-        this.helpMenuItems = [
-            { title: 'About', component: 'page-about', icon: 'information-circle' },
-            { title: 'Support', component: 'page-support', icon: 'call' },
-            { title: 'App Settings', component: 'page-settings', icon: 'cog' },
-            { title: 'Walkthrough', component: 'page-walkthrough', icon: 'photos' }
-        ];
-
     }
 
     initializeApp() {
@@ -91,8 +64,6 @@ export class foodIonicApp {
     }
 
     openPage(page) {
-        // Reset the content nav to have just this page
-        // we wouldn't want the back button to show in this scenario
         this.nav.setRoot(page.component);
     }
 }
