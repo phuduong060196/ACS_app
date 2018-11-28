44f98a7b66281a2dfa3536ec3f2408240429e24f
PhuDuong <phuduong060196@gmail.com>
2018-11-28 16:31:21 +0700

    nothing

--- MobileCap/src/app/app.component.ts
+++ MobileCap/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform, ToastController, App, AlertController } from 'ionic-angular';
+import { Nav, Platform, ToastController, App, AlertController, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
