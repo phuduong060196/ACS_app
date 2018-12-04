506b77235dad9c2dc09a3f2d72255476dea2b9e2
PhuDuong <phuduong060196@gmail.com>
2018-12-04 20:01:49 +0700

    edit button 'Xac nhan' location

--- MobileCap/src/pages/change-location/change-location.ts
+++ MobileCap/src/pages/change-location/change-location.ts
@@ -6,70 +6,83 @@ import { LocalHelperProvider } from '../../providers/local-helper/local-helper';
 declare var google;
 
 @IonicPage({
-  name: 'page-change-location',
-  segment: 'change-location'
+    name: 'page-change-location',
+    segment: 'change-location'
 })
 
 @Component({
-  selector: 'page-change-location',
-  templateUrl: 'change-location.html',
+    selector: 'page-change-location',
+    templateUrl: 'change-location.html',
 })
 
 
 export class ChangeLocationPage {
 
-  GoogleAutocomplete;
-  autocomplete;
-  autocompleteItems;
-  geocoder;
+    GoogleAutocomplete;
+    autocomplete;
+    autocompleteItems;
+    geocoder;
+    latC: any;
+    lngC: any;
+    yourLocationC: any;
 
-  constructor(public navCtrl: NavController, public zone: NgZone, private localPro: LocalHelperProvider) {
-    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
-    this.autocomplete = { input: '' };
-    this.autocompleteItems = [];
-    this.geocoder = new google.maps.Geocoder;
-  }
-
-  updateSearchResults() {
-    if (this.autocomplete.input == '') {
-      this.autocompleteItems = [];
-      return;
-    }
-    this.GoogleAutocomplete.getPlacePredictions(
-      { input: this.autocomplete.input },
-      (predictions, status) => {
+    constructor(public navCtrl: NavController, public zone: NgZone, private localPro: LocalHelperProvider) {
+        this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+        this.autocomplete = { input: '' };
         this.autocompleteItems = [];
-        this.zone.run(() => {
-          if (predictions != null) {
-            predictions.forEach((prediction) => {
-              this.autocompleteItems.push(prediction);
-            });
-          }
-          else {
+        this.geocoder = new google.maps.Geocoder;
+    }
+
+    updateSearchResults() {
+        if (this.autocomplete.input == '') {
             this.autocompleteItems = [];
-          }
-        });
-      }
-    );
-  }
-
-  selectSearchResult(item) {
-    console.log(item);
-    this.autocomplete.input = item.description;
-    this.autocompleteItems = [];
-    this.geocoder.geocode({ 'placeId': item.place_id }, (results, status) => {
-      if (status === 'OK' && results[0]) {
-        this.localPro.SetLocation = {
-          lat: results[0].geometry.location.lat(),
-          lng: results[0].geometry.location.lng(),
-          yourLocation: item.description
+            return;
         }
-      }
-    });
-  }
+        this.GoogleAutocomplete.getPlacePredictions(
+            { input: this.autocomplete.input },
+            (predictions, status) => {
+                this.autocompleteItems = [];
+                this.zone.run(() => {
+                    if (predictions != null) {
+                        predictions.forEach((prediction) => {
+                            this.autocompleteItems.push(prediction);
+                        });
+                    }
+                    else {
+                        this.autocompleteItems = [];
+                    }
+                });
+            }
+        );
+    }
 
-  closeModal() {
-    this.navCtrl.pop();
-  }
+    selectSearchResult(item) {
+        console.log(item);
+        this.autocomplete.input = item.description;
+        this.autocompleteItems = [];
+        this.geocoder.geocode({ 'placeId': item.place_id }, (results, status) => {
+            if (status === 'OK' && results[0]) {
+                this.latC = results[0].geometry.location.lat();
+                this.lngC = results[0].geometry.location.lng();
+                this.yourLocationC = item.description;
+            }
+        });
+    }
+
+    closeModal() {
+        this.navCtrl.pop();
+    }
+
+    confirmAction() {
+        console.log(this.latC + ' ' + this.lngC + ' ' + this.yourLocationC);
+        if (this.latC && this.lngC && this.yourLocationC) {
+            this.localPro.SetLocation = {
+                lat: this.latC,
+                lng: this.lngC,
+                yourLocation: this.yourLocationC
+            }
+        }
+        this.navCtrl.pop();
+    }
 
 }
